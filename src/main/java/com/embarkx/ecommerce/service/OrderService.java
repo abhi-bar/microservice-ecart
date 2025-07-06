749402991f0fc65a52084a0bde7975e8265a608d
Abinash Barman <abin90901@gmail.com>
2025-07-07 02:37:57 +0530

    Commit with changes for Product + Order class

---
+++ src/main/java/com/embarkx/ecommerce/service/OrderService.java
@@ -0,0 +1,10 @@
+package com.embarkx.ecommerce.service;
+
+import com.embarkx.ecommerce.payload.OrderDTO;
+import jakarta.transaction.Transactional;
+
+public interface OrderService {
+
+    @Transactional
+    OrderDTO placeOrder(String emailId, Long addressId, String paymentMethod, String pgName, String pgPaymentId, String pgStatus, String pgResponseMessage);
+}
